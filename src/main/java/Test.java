925e1149e3cc621755d3c820dcf358b503cb8e98
PatrickCelentano <patrick_celentano@yahoo.com>
2017-09-19 21:28:09 -0400

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+import form.score.LeadSheet;
+import properties.sound.Pitch;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("TEST");
+        Pitch pitch;
+        LeadSheet leadSheet;
+    }
+}
